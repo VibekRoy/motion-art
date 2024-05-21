a49a4e2faa5b868231db7fb5333095a91e33905b
VibekRoy <97304965+VibekRoy@users.noreply.github.com>
2024-05-22 00:27:18 +0530

    Final

---
+++ src/components/Content.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div id='Content'>
+        <div className='Content-left'>
+            <div className='Content-left-1'>Transform Your Website</div>
+            <div className='Content-left-2'>With Motion Art Effect</div>
+        </div>
+
+        <div className="Content-right">
+            <div className="Content-right-1">Attract Your Visitors Attention With Colorful <span>Motion Art Effect</span></div>
+            <div className="Content-right-2">Unleash the power of creativity with Motion Art for Elementor - your ultimate solution for seamlessly integrating captivating animations into your website.</div>
+        </div>
+        
+        </div>
+  )
+}
+
+export default Content
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div id='nav'>
+        <a href="https://qodematrix.com/motion-art-for-elementor/">
+        <img src="/assets/MotionArtEffect-logo.png" alt="logo" />
+        </a>
+
+        <a href="https://codecanyon.net/item/motion-art-for-elementor-wordpress-plugin/48826891">
+        <div className='purchase-button'>Purchase Now</div>
+        </a>
+    </div>
+  )
+}
+
+export default Navbar
